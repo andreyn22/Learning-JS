2cda140ad93e552e41273a78d5bdf9f71db9e250
Andrei Negura <andrei.negura@endava.com>
2025-09-07 00:28:18 +0300

    TypeScript vs JavaScript

---
+++ lessons/lesson10.ts
@@ -0,0 +1,17 @@
+// var customerFirstName = "John";
+// var customerLastName = "Doe";
+// var customerAge = 30;
+
+
+var customerFirstName: string = "John";
+var customerLastName: string = "Doe";
+var customerAge: number = 30;
+
+type Customer = {firstName: string, lastName: string, active:boolean, age:number};
+
+var firstCustomer: Customer = {
+    firstName: "Alex",
+    lastName: "Novak",
+    active: true,
+    age: 25
+}
